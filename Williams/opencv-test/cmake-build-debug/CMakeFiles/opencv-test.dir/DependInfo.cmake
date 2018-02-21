58626aa670d91b867f93830ddb85c81981f9c9cc
williamswhy <why980429@gmail.com>
2018-02-21 16:23:27 -0500

    update cmake version

--- Williams/opencv-test/cmake-build-debug/CMakeFiles/opencv-test.dir/DependInfo.cmake
+++ Williams/opencv-test/cmake-build-debug/CMakeFiles/opencv-test.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/why/CLionProjects/opencv-test/main.cpp" "/home/why/CLionProjects/opencv-test/cmake-build-debug/CMakeFiles/opencv-test.dir/main.cpp.o"
+  "/home/why/computer-vision/Williams/opencv-test/main.cpp" "/home/why/computer-vision/Williams/opencv-test/cmake-build-debug/CMakeFiles/opencv-test.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- Williams/opencv-test2/cmake-build-debug/CMakeFiles/opencv_test2.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for opencv_test2.
-# This may be replaced when dependencies are built.
