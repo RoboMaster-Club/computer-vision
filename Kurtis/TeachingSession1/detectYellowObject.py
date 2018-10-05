a9649f0e872ca3a1ff66d9e29dc37757bde186dc
kdcrafter <kurtisdoubled@hotmail.com>
2018-10-05 15:14:59 -0600

    fine tunned the object detection

--- Kurtis/TeachingSession1/detectYellowObject.py
+++ Kurtis/TeachingSession1/detectYellowObject.py
@@ -15,31 +15,30 @@ def isolateElement(src, mask):
 
     #isolate element
     object = cv2.bitwise_and(src, src, mask=mask)
-    cv2.imshow('After Masking', object)
+    #cv2.imshow('After Masking', object)
 
     #blur
     objectBGR = cv2.cvtColor(object, cv2.COLOR_HSV2BGR)
     objectBlur = cv2.GaussianBlur(objectBGR, (5, 5), 0, 0)
-    cv2.imshow('After Blur', objectBlur)
+    #cv2.imshow('After Blur', objectBlur)
 
     #thresholding
-    _, objectThresh = cv2.threshold(objectBlur, 60, 255, cv2.THRESH_BINARY)
-    cv2.imshow('After Thresholding', objectBlur)
+    _, objectThresh = cv2.threshold(objectBlur, 100, 255, cv2.THRESH_BINARY)
+    #cv2.imshow('After Thresholding', objectThresh)
 
     return objectThresh
 
-def detectShape(contour):
+def simplifyShape(contour):
     """
-    find the shape of a contour
+    simplyfy the shape of a contour by decreasing the number of vertices
     :param contour: a single contour from findContours()
-    :return numSides: the number of sides of the shape
-    :return vertices: the vertices of the shape
+    :return vertices: the new vertices of the contour
     """
 
     arcLength = cv2.arcLength(contour, True)
-    vertices = cv2.approxPolyDP(contour, 0.06*arcLength, True)
+    vertices = cv2.approxPolyDP(contour, 0.01*arcLength, True)
 
-    return len(vertices), vertices
+    return vertices
 
 def main():
     img = cv2.imread('image_2.jpg')
@@ -55,15 +54,15 @@ def main():
     objectGrey = cv2.cvtColor(object, cv2.COLOR_BGR2GRAY)
     _, objectContours, _ = cv2.findContours(objectGrey, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    for countour in objectContours:
-        numSides, verticies = detectShape(countour)
+    #draw rectangles around each circle
+    for contour in objectContours:
+        vertices = simplifyShape(contour)
 
-        if numSides >= 4: #if its a circle
-            cv2.drawContours(img, [countour], -1, (0, 0, 0), 1)
+        if len(vertices) >= 9: #if it's a circle
+            cv2.drawContours(img, [contour], -1, (0, 0, 0), 1)
 
-            #draw rectangle around the object
-            (x, y, w, h) = cv2.boundingRect(verticies)
-            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 0), 1)
+            (x, y, w, h) = cv2.boundingRect(vertices)
+            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 0), 2)
 
 
     cv2.imshow('After Object Detection', img)
